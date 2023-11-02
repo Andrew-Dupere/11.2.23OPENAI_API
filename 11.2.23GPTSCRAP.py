76fb9f1c6130b308b01909d309c98bf679f21c79
andrewdupere <andrewdupere@outlook.com>
2023-11-02 10:01:05 -0400

    gptscrap

--- 11.2.23GPTSCRAP.py
+++ 11.2.23GPTSCRAP.py
@@ -9,29 +9,29 @@ load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-model = "text-davinci-003"
+import openai
 
-def prompt(prompt):
-  
-  response = openai.Completion.create(
-  model=model,
-  prompt=(prompt)
-).choices[0].text
-  return response
+def chat_with_gpt(model):
+    print(f'GPT <{model}>|:::')
 
-print(prompt(input(f'GPT <{model}>|')))
-
-def contin(prompt):
-    model = model
-    print(f'GPT <{model}>|')
     while True:
-        userInput = input('GPT:::')
+        userInput = input(f'You: ')
+        
+        if userInput.lower() == "x":
+            print(f'GPT <{model}>| Chat ended.')
+            break
+        
         response = prompt(userInput, model)
         print(f'GPT <{model}>| {response}')
 
-'_________________________________________________'
+def prompt(prompt, model):
+    response = openai.Completion.create(
+        model=model,
+        prompt=prompt
+    ).choices[0].text
+    return response
 
 if __name__ == "__main__":
-    model = "text-davinci-003"  # Define your GPT model here
-    contin(model)
+    model = "text-davinci-003"  
+    chat_with_gpt(model)
 
