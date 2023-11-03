1362ce846b1c6b1abc14ca09ae3cdb29b1f0d1b8
andrewdupere <andrewdupere@outlook.com>
2023-11-03 09:30:53 -0400

    gptscrap

--- 11.2.23GPTSCRAP.py
+++ 11.2.23GPTSCRAP.py
@@ -2,27 +2,10 @@ import os
 import openai
 import dotenv
 #.env contains 'OPEN_API_KEY'
-
 from dotenv import load_dotenv
 load_dotenv()
 
-
 openai.api_key = os.getenv('OPENAI_API_KEY')
-
-import openai
-
-def chat_with_gpt(model):
-    print(f'GPT <{model}>|:::')
-
-    while True:
-        userInput = input(f'You: ')
-        
-        if userInput.lower() == "x":
-            print(f'GPT <{model}>| Chat ended.')
-            break
-        
-        response = prompt(userInput, model)
-        print(f'GPT <{model}>| {response}')
 
 def prompt(prompt, model):
     response = openai.Completion.create(
@@ -31,7 +14,47 @@ def prompt(prompt, model):
     ).choices[0].text
     return response
 
-if __name__ == "__main__":
-    model = "text-davinci-003"  
-    chat_with_gpt(model)
+
+def chat_with_gpt(model):
+    print(f'Mode: {model} chat starting')
+
+    while True:
+        userInput = input(f'<{model}>::: ')
+        
+        if userInput.lower() == "x":
+            print(f'GPT <{model}>| GoodBye')
+            break
+        
+        response = prompt(userInput, model)
+        print(f'GPT <{model}>| {response}')
+
+def gptCompletions():
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"},
+            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}
+        ]
+    )
+    print(response)
+
+def picGen():    
+    response = openai.Image.create(
+        prompt=str(input('What do you want to see?')),
+        n=1,
+        size="1024x1024"
+        )
+    image_url = response['data'][0]['url']
+
+    print(image_url)
+
+
+chat_with_gpt(model='text-davinci-003')
+
+# picGen()
+
+#gptCompletions()
 
